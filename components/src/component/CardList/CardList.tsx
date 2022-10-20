13cae5729722d8dca1aa089a51ce2ee0658ffdaf
Katherina Kaliaha <mellinda077@mail.ru>
2022-10-20 12:24:24 +0300

    refactor: change Cardlist component from class component to function component

--- components/src/component/CardList/CardList.tsx
+++ components/src/component/CardList/CardList.tsx
@@ -3,20 +3,12 @@ import { ICardApi } from 'data/interfaces';
 import { CardListProps } from 'data/types';
 import React from 'react';
 
-class CardList extends React.Component<CardListProps> {
-  constructor(props: CardListProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="card-list" onClick={this.props.openModalWindow}>
-        {this.props.data.map((card: ICardApi) => (
-          <Card movie={card} key={card.id} />
-        ))}
-      </div>
-    );
-  }
+export function CardList({ data, openModalWindow }: CardListProps) {
+  return (
+    <div className="card-list" onClick={openModalWindow}>
+      {data.map((card: ICardApi) => (
+        <Card movie={card} key={card.id} />
+      ))}
+    </div>
+  );
 }
-
-export { CardList };
